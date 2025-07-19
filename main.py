294cf53a5791c3e754e13027484f867541df1157
AleLode <lodetti.alessandro02@gmail.com>
2025-07-19 11:31:45 +0200

    altre funzioni per il salvataggio in locale dei modelli

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from guardrails_project.Util.instantiateModelAndFillFile import instantiateModel
 from guardrails_project.Util.runGuardrail import runGuardrail
 
 if __name__ == "__main__":
+    """
+    Main entry point for the script. It allows the user to choose between running a model or testing responses.
+    The user can instantiate a model and fill a file with responses or run guardrail tests on existing responses.
+    The script will continue to prompt the user for actions until they choose to exit.
+    """
+
     
     while True:
         print("Do you want to instantiate a LLM and run it or do you want to run the tests? (llm/test)  ")
@@ -21,4 +27,6 @@ if __name__ == "__main__":
         if another_operation != 'y':
             print("Exiting the program.")
             break
+
+    print("Thank you for using the script!")
         
